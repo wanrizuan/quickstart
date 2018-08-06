4920211dc80b6ef69c40aa93b8b26384996cea1f
wan rizuan wan mamat <wan.rizuan@3fresources.com>
2018-08-06 17:01:11 +0800

    First Training

---
+++ app/Http/routes.php
@@ -0,0 +1,26 @@
+<?php
+
+/*
+|--------------------------------------------------------------------------
+| Application Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register all of the routes for an application.
+| It's a breeze. Simply tell Laravel the URIs it should respond to
+| and give it the controller to call when that URI is requested.
+|
+*/
+
+use App\Task;
+use Illuminate\Http\Request;
+
+Route::group(['middleware' => 'web'], function () {
+
+	/**
+	 * Show Task Dashboard
+	 */
+	Route::get('/', function () {
+	    return view('welcome');
+	});
+
+});
